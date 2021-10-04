d7905c9c70f09ab069dea7936e072d86dd3485cc
τєαм ϰмαяτγ <85209355+S780821@users.noreply.github.com>
2021-10-04 07:34:15 +0530

    Update userbotjoin.py

--- VCPlayBot/modules/userbotjoin.py
+++ VCPlayBot/modules/userbotjoin.py
@@ -24,7 +24,7 @@ async def addchannel(client, message):
     try:
         user = await USER.get_me()
     except:
-        user.first_name = "VCPlayBot"
+        user.first_name = "XmartyBot"
 
     try:
         await USER.join_chat(invitelink)
@@ -37,7 +37,7 @@ async def addchannel(client, message):
         print(e)
         await message.reply_text(
             f"<b>🛑 Flood Wait Error 🛑 \n User {user.first_name} couldn't join your group due to heavy join requests for userbot! Make sure user is not banned in group."
-            "\n\nOr manually add @VCPlayAssistant to your Group and try again</b>",
+            "\n\nOr manually add @XmartyAssistant to your Group and try again</b>",
         )
         return
     await message.reply_text(
