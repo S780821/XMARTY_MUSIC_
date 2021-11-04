89ed079a7ec683272cd11d09c7fd2297efa5cd39
τєαм ϰмαяτγ <85209355+S780821@users.noreply.github.com>
2021-11-04 21:15:35 +0530

    Update __init__.py

--- XmartyMusic/function/__init__.py
+++ XmartyMusic/function/__init__.py
@@ -14,6 +14,6 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-from XmartyMuisc.function.admins import admins, get, set
+from XmartyMusic.function.admins import admins, get, set
 
 __all__ = ["set", "get", "admins"]
