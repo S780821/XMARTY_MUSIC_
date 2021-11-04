742f9fa724cf6993f305168e21b726008ec1d519
τєαм ϰмαяτγ <85209355+S780821@users.noreply.github.com>
2021-11-04 20:57:32 +0530

    Create __init__.py

---
+++ XmartyMusic/services/converter/__init__.py
@@ -0,0 +1,23 @@
+# XmartyMusic (Telegram bot project )
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+from os import listdir, mkdir
+
+if "raw_files" not in listdir():
+    mkdir("raw_files")
+
+from XmartyMusic.services.converter.converter import convert
+
+__all__ = ["convert"]
