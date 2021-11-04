cab1a93950a62fe9c66fdc3b16f3876194c997ff
τєαм ϰмαяτγ <85209355+S780821@users.noreply.github.com>
2021-11-04 20:50:32 +0530

    Add files via upload

---
+++ XmartyMusic/function/admins.py
@@ -0,0 +1,29 @@
+# XmartyMusic (Telegram bot project )
+# Copyright (C) 2021  Xmartperson
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+from typing import Dict, List
+
+admins: Dict[int, List[int]] = {}
+
+
+def set(chat_id: int, admins_: List[int]):
+    admins[chat_id] = admins_
+
+
+def get(chat_id: int) -> List[int]:
+    if chat_id in admins:
+        return admins[chat_id]
+    return []
